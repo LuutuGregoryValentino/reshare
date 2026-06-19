67b4e99969a03b32ff3865bf0814efa22be2919a
LuutuGregoryValentino <luutugregory@gmail.com>
2026-06-20 01:01:37 +0300

    feat:configure connection manger and create connectionmanager instance

---
+++ backend/app/services/session.py
@@ -0,0 +1,33 @@
+
+from fastapi import WebSocket
+from typing import Dict
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: Dict[str: WebSocket] = {}
+
+    async def accept_device(self, device_id: str, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections[device_id] = websocket
+        print(f"Memory Cache updated. Active devices: \n{list(self.active_connections.keys())}")
+
+    def remove_device(self, device_id:str):
+        if device_id in self.active_connections:
+            del self.active_connections[device_id]
+            print(f"Device {device_id} has been gracefully disconnected\nRemaining Devices: \n{list(self.active_connections.keys())}")
+
+        
+    async def send_target_messages(self, message: str, target_device_id: str) -> bool:
+        if target_device_id in self.active_connections:
+            target_websocket = self.active_connections[target_device_id]
+
+            await target_websocket.send_text(message)
+            return True
+        
+        print (f"Failed to Route message. target device {target_device_id} is")
+        return False
+
+
+
+
+manager = ConnectionManager() # creates one global instance of the active connection list in RAM
